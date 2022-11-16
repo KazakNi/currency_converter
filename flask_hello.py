01354f14b2f7539813346dd0c795f891054264c8
Nikita Kazakov <kazakov.ni@yandex.ru>
2022-11-17 01:00:05 +0300

    v1

--- flask_hello.py
+++ flask_hello.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_user_profile(username):
+    return f'User {escape(username)}'
+
 @app.route("/")
+def index() -> str:
+    return "<h4>Index Page</h4>"
+
+@app.route("/hello/")
 def hello_world() -> str:
     return "<p>Hello, world!</p>"
