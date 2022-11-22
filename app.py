bf81ff4dc0afc2a7134f6271900e69f2819fd596
Nikita Kazakov <kazakov.ni@yandex.ru>
2022-11-22 19:43:03 +0300

    db

--- app.py
+++ app.py
@@ -41,3 +41,33 @@ def get_widgets():
     cursor.close()
 
     return json.dump(json_data)
+
+@app.route("/initdb")
+def db_init():
+    mydb = mysql.connector.connect(
+        host="mysqldb",
+        user="root",
+        password="p@ssw0rd1"
+    )
+    cursor = mydb.cursor()
+
+    cursor.execute("DROP DATABASE IF EXISTS inventory")
+    cursor.execute("CREATE DATABASE inventory")
+    cursor.close()
+
+    mydb = mysql.connector.connect(
+        host="mysqldb",
+        user="root",
+        password="p@ssw0rd1",
+        database="inventory"
+    )
+    cursor = mydb.cursor()
+
+    cursor.execute("DROP TABLE IF EXISTS widgets")
+    cursor.execute("CREATE TABLE widgets (name VARCHAR(255), description VARCHAR(255))")
+    cursor.close()
+
+    return 'init database'
+
+if __name__ == "__main__"
+app.run(host="0.0.0.0")
