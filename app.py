9047ad43f7739519c3b538adef93b93a434653ce
Nikita Kazakov <kazakov.ni@yandex.ru>
2022-12-03 23:32:33 +0300

    rendering template

--- app.py
+++ app.py
@@ -1,73 +1,14 @@
 import mysql.connector
 import json
-from flask import Flask
+from flask import Flask, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route('/user/<username>')
 def show_user_profile(username):
-    return f'User {escape(username)}'
+    return render_template('index.html', title='hello world')
 
-@app.route('/post/<int:post_id>')
-def show_posts(post_id):
-    return f'Post {escape(post_id)}'
-
-@app.route("/")
-def index() -> str:
-    return "<h4>Index Page</h4>"
-
-@app.route("/hello/")
-def hello_world() -> str:
-    return "<p>Hello, world!</p>"
-
-@app.route('/widgets')
-def get_widgets():
-    mydb = mysql.connector.connect(
-        host="mysqldb",
-        user="root",
-        password="p@ssw0rd1",
-        database="inventory"
-    )
-    cursor = mydb.cursor()
-
-    cursor.execute("SELECT * FROM widgets")
-
-    row_headers = [x[0] for x in cursor.description]
-    results = cursor.fetchall()
-    json_data = []
-    for result in results:
-        json_data.append(dict(zip(row_headers, result)))
-    cursor.close()
-
-    return json.dump(json_data)
-
-@app.route("/initdb")
-def db_init():
-    mydb = mysql.connector.connect(
-        host="mysqldb",
-        user="root",
-        password="p@ssw0rd1"
-    )
-    cursor = mydb.cursor()
-
-    cursor.execute("DROP DATABASE IF EXISTS inventory")
-    cursor.execute("CREATE DATABASE inventory")
-    cursor.close()
-
-    mydb = mysql.connector.connect(
-        host="mysqldb",
-        user="root",
-        password="p@ssw0rd1",
-        database="inventory"
-    )
-    cursor = mydb.cursor()
-
-    cursor.execute("DROP TABLE IF EXISTS widgets")
-    cursor.execute("CREATE TABLE widgets (name VARCHAR(255), description VARCHAR(255))")
-    cursor.close()
-
-    return 'init database'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
