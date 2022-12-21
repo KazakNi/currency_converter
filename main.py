2ecba12e9e3e9009fd52585e6324543759dc7abf
Nikita Kazakov <kazakov.ni@yandex.ru>
2022-12-21 20:35:21 +0300

    Query topic

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+from typing import Union
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return {"message": "Hello World"}
+@app.get('/users/{user_id}/items/{item_id}')
+async def read_item(user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return Item
